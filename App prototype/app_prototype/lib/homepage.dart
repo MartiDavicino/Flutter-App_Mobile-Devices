46c76fbb073af50ec836c4bc8487c64ffaf72111
MartiDavicino <60965161+MartiDavicino@users.noreply.github.com>
2021-12-20 16:19:30 +0100

    Clean up and home page

---
+++ App prototype/app_prototype/lib/homepage.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Burger Collection"),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          children: [
+            Header("Burger Collection"),
+            HomePageButton("About Us"),
+            HomePageButton("Catalogue"),
+            HomePageButton("Telmo link"),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomePageButton extends StatelessWidget {
+  final String text;
+  const HomePageButton(
+    this.text, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      width: 200,
+      margin: EdgeInsets.fromLTRB(0, 60, 0, 60),
+      child: FlatButton(
+        color: Colors.cyan,
+        onPressed: () {},
+        child: Text(
+          text,
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  final String text;
+  const Header(
+    this.text, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(0, 70, 0, 90),
+      child: Text(
+        text,
+        style: const TextStyle(fontSize: 50, fontWeight: FontWeight.w700),
+      ),
+    );
+  }
+}
--- App prototype/app_prototype/lib/main.dart
+++ App prototype/app_prototype/lib/main.dart
@@ -1,89 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:app_prototype/homepage.dart';
 
-void main() => runApp(MaterialApp(
-      title: "Burger Collection",
-      home: Catalogue(),
+//import 'package:firebase_core/firebase_core.dart';
+//const MaterialColor myColor = Colors.brown;
+//const Color primaryColor = Colors.brown;
+//const Color secondaryColor = Colors.orange;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-    ));
-
-class Catalogue extends StatefulWidget {
-  @override
-  State<Catalogue> createState() => _CatalogueState();
-}
-
-class _CatalogueState extends State<Catalogue> {
-  @override
-  void initState() {
-    super.initState();
-    fetchData();
-  }
-
-  fetchData() async {}
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Burger Collection"),
-        backgroundColor: Colors.orange,
-      ),
-      body: GridView.count(crossAxisCount: 2, children: [
-        BurgerCard("Burger #1"),
-        BurgerCard("Burger #2"),
-        BurgerCard("Burger #3"),
-        BurgerCard("Burger #4"),
-        BurgerCard("Burger #5"),
-        BurgerCard("Burger #6"),
-        BurgerCard("Burger #7"),
-        BurgerCard("Burger #8"),
-      ]),
-      drawer: Drawer(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.red,
-        child: Icon(Icons.refresh),
-      ),
-    );
-  }
-}
-
-class BurgerCard extends StatelessWidget {
-  final String name;
-  const BurgerCard(
-    this.name, {
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.all(50),
-            height: 150,
-            width: 150,
-            color: Colors.green,
-          ),
-          Expanded(
-            child: Container(),
-          ),
-          Container(
-            alignment: Alignment.center,
-            padding: EdgeInsets.all(10),
-            height: 100,
-            width: 350,
-            child: Text(
-              name,
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          )
-        ],
-      ),
+      title: "Burger App",
+      // theme: ThemeData(
+      //   primarySwatch: myColor,
+      // ),
+      home: HomePage(),
     );
   }
 }
